506164ad815a4a9f3b01f032de776a72e22b1761
yk-jp <k82.y12@gmail.com>
2022-02-03 22:08:59 -0800

    this is a latest version

--- Mini-Project(Tic-Tac-Toe)-pair-work/game.py
+++ Mini-Project(Tic-Tac-Toe)-pair-work/game.py
@@ -4,26 +4,23 @@ has_winner = False
 player_symbol, computer_symbol = define_symbols()
 
 
-patterns = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [1, 4, 7],
-    [2, 5, 8],
-    [3, 6, 9],
-    [1, 5, 9],
-    [3, 5, 7],
-]
-
 my_board = [
     ["1", "2", "3"],
     ["4", "5", "6"],
     ["7", "8", "9"]
 ]
 
-while not has_winner:
-    print_board(my_board)
+target_list = []
 
+while not has_winner:
+
+    if len(available_moves(my_board)) == 0:
+        print_board(my_board)
+        print("It's a draw!")
+        has_winner = True
+        break
+
+    print_board(my_board)
     player_choice = input("Please make your move! [1-9]: ")
 
     if not player_choice.isdigit() or int(player_choice) < 1 or int(player_choice) > 9:
@@ -36,17 +33,14 @@ while not has_winner:
         print_board(my_board)
         print("Congratulations! You won!")
         has_winner = True
+        break
 
-    best_position = define_movement(int(player_choice), my_board, patterns)
-    print(f"Best position = {best_position}")
+    best_position = define_movement(int(player_choice), my_board, computer_symbol, target_list)
+    target_list.append(best_position)
     select_space(my_board, best_position, computer_symbol)
 
     if has_won(my_board, computer_symbol):
         print_board(my_board)
         print("Sorry, Computer won!")
         has_winner = True
-
-    if len(available_moves(my_board)) == 0:
-        print_board(my_board)
-        print("It's a draw!")
-        has_winner = True
+        break
--- Mini-Project(Tic-Tac-Toe)-pair-work/tic_tac_toe.py
+++ Mini-Project(Tic-Tac-Toe)-pair-work/tic_tac_toe.py
@@ -1,32 +1,35 @@
 import random
 
+patterns = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 5, 9],
+    [3, 5, 7],
+]
+
 
 def define_symbols() -> (str, str):
+    """
+    This function define which symbol will be used by player and computer
+    :return: (str, str)
+    """
     symbols = ["X", "O"]
     random.shuffle(symbols)
-
     print("Tic-Tac-Toe!")
     print(f"Player is {symbols[0]} and Computer is {symbols[1]}")
-
     return symbols[0], symbols[1]
 
 
-def define_movement(player_choice: int, board: [[int]], pattern: [[int]]) -> int:
-    temp_list = []
-
-    for idx in pattern:
-        if player_choice in idx:
-            idx.remove(player_choice)
-            temp_list.append(idx)
-
-    x = random.randint(0, len(temp_list) - 1)
-    y = random.randint(0, len(idx) - 1)
-
-    best_position = temp_list[x][y]
-    return best_position
-
-
-def print_board(board):
+def print_board(board: [[int]]) -> None:
+    """
+    This function prompt board to player
+    :param board: Game board
+    :return: None
+    """
     print()
     print(" " + board[0][0] + " | " + board[0][1] + " | " + board[0][2] + " ")
     print("---|---|---")
@@ -36,20 +39,100 @@ def print_board(board):
     print()
 
 
-def select_space(board, move, turn):
-    if move not in range(1, 10):
-        return False
-    row = int((move - 1) / 3)
-    col = (move - 1) % 3
-    if board[row][col] != "X" and board[row][col] != "O":
-        board[row][col] = turn
-        return True
+def get_best_position_win(_, board, computer_symbol) -> (bool, [int]):
+    """
+    This function returns the best movement to computer win
+    :param _: (Not used) possible movements
+    :param board: Game board
+    :param computer_symbol: symbol that identify computer
+    :return: (bool, [int]) bool is used to an attack movement and best possible movement
+    """
+    can_win = False
+    best_win_position = None
+
+    for idx in patterns:
+        occurrences = 0
+        for jdx in idx:
+            row = int((jdx - 1) / 3)
+            col = (jdx - 1) % 3
+
+            if board[row][col] == computer_symbol:
+                occurrences += 1
+
+        if occurrences == 2:
+            can_win = True
+            best_win_position = idx
+            return can_win, best_win_position
+    return can_win, best_win_position
+
+
+def define_movement(player_choice: int, board: [[int]], computer_symbol: str, target_list: []) -> int:
+    """
+    This function define which board's position should computer choose
+    :param computer_symbol: Symbol that represents a computer in board
+    :param player_choice: Last player movement
+    :param board: Game board
+    :return: Best position to computer move
+    """
+    temp_list = []
+    best_position = None
+
+    for idx in patterns:
+        if player_choice in idx:
+            if len(idx) == 0:
+                temp_list = temp_list.remove(idx)
+
+            idx.remove(player_choice)
+
+            for jdx in target_list:
+                if jdx in idx:
+                    idx.remove(jdx)
+
+            temp_list.append(idx)
+
+    possible_win, best_win_position = get_best_position_win(temp_list, board, computer_symbol)
+
+    if possible_win:
+        for idx in best_win_position:
+            if idx in available_moves(board):
+                best_position = idx
     else:
-        print("Invalid move! Please try again!")
-        return False
+        random_vertical = random.randint(0, len(temp_list) - 1)
+        random_horizontal = 0
+
+        if random_horizontal not in available_moves(board):
+            best_position = temp_list[random_vertical][random_horizontal]
+        else:
+            random_horizontal = 1
+            best_position = temp_list[random_vertical][random_horizontal]
+
+    return best_position
 
 
-def available_moves(board):
+def select_space(board: [[int]], move: int, turn) -> bool:
+    """
+    This function marks a position in board to player or computer
+    :param board: Game board
+    :param move: Position to be marked
+    :param turn: Who is requesting
+    :return: Return false case a position cannot be marked or if invalid. Return true if marked
+    """
+    if move is not None:
+        row = int((move - 1) / 3)
+        col = (move - 1) % 3
+        if board[row][col] != "X" and board[row][col] != "O":
+            board[row][col] = turn
+            return True
+    print("Invalid move! Please try again!")
+    return False
+
+
+def available_moves(board: [[int]]) -> [int]:
+    """
+    This function returns all available movements that a player can use
+    :param board: Game board
+    :return: List with all available positions
+    """
     moves = []
     for row in board:
         for col in row:
@@ -58,7 +141,13 @@ def available_moves(board):
     return moves
 
 
-def has_won(board, player):
+def has_won(board: [[int]], player: str) -> bool:
+    """
+    This function checks if a player has won
+    :param board: Game board
+    :param player: Who is requesting - player or computer
+    :return: Returns false case not and true case yes
+    """
     for row in board:
         if row.count(player) == 3:
             return True
